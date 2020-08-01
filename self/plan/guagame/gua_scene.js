aac7e89e5f2fe2663f52023ed4b2d5c55259e394
wwz <you@example.com>
2020-08-01 11:40:53 +0800

    'ke7'

---
+++ self/plan/guagame/gua_scene.js
@@ -0,0 +1,23 @@
+class GuaScene {
+    constructor(game) {
+        this.game = game
+        this.elements = []
+    }
+    static new(game) {
+        var i = new this(game)
+        return i
+    }
+    draw() {
+        for (let i = 0; i < this.elements.length; i++) {
+            let e = this.elements[i]
+            this.game.drawImage(e)
+        }
+    }
+    addElement(img) {
+        this.elements.push(img)
+    }
+
+    update() {
+
+    }
+}
